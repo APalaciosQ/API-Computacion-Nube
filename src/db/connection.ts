20ccc3aa966946cfb2fd9979916bf6fc7b82dde4
APalaciosQ <93203495+APalaciosQ@users.noreply.github.com>
2024-05-17 01:00:24 -0400

    Chance database password

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize' 
 
-const sequelize = new Sequelize('almacen', 'postgres', 'Celina$957', {
+const sequelize = new Sequelize('almacen', 'postgres', '12345', {
     host: 'localhost',
     dialect: 'postgres'
   });
